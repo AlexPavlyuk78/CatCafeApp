6c9d590a57493fadc9deb520c93be6061864e71f
Alex78ol <119539530+Alex78ol@users.noreply.github.com>
2023-05-08 20:28:45 +0200

    Set up last page.

--- CatCafeApp/FirstViewController.swift
+++ CatCafeApp/FirstViewController.swift
@@ -13,31 +13,13 @@ class FirstViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     
-    @IBOutlet weak var deleteButton: UIButton!
-    @IBOutlet weak var tappingButton: UIButton!
     
-    
-    @IBOutlet weak var itemCart: UIImageView!
-    
-    var cartItems = ["Coffee", "Milc", "Banana"] // array to store cart items
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tappingButton.layer.cornerRadius = 8
-        deleteButton.layer.cornerRadius = 8
         
-        
-    }
-    
-    
-    @IBAction func addItemToTheCart(_ sender: UIButton) {
-    }
-    
-    
-    
-    func deleteItemFromtheCart(_ sender: UIButton) {
         
     }
 }
