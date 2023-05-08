fbeb154f728f4d59ff60afed7443817eac56d212
Alex78ol <119539530+Alex78ol@users.noreply.github.com>
2023-05-08 13:03:00 +0200

    Button constrain set up

--- CatCafeApp/FirstViewController.swift
+++ CatCafeApp/FirstViewController.swift
@@ -9,4 +9,20 @@ import UIKit
 
 class FirstViewController: UIViewController {
     
+   
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBOutlet weak var tappingButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tappingButton.layer.cornerRadius = 8
+
+  }
+    
+    @IBAction func tappingButton(_ sender: UIButton) {
+    }
+    
 }
--- CatCafeApp/StartViewController.swift
+++ CatCafeApp/StartViewController.swift
@@ -8,32 +8,47 @@
 import UIKit
 
 class StartViewController: UIViewController {
-
     
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        guard let tabBarVC = segue.destination as? UITabBarController else { return }
-        guard let viewControllers = tabBarVC.viewControllers else { return }
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var buttonTapped: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
-        viewControllers.forEach { viewController in
-            if let firstVC = viewController as? FirstViewController {
-                firstVC.view.backgroundColor = .systemMint
-            } else if let secondVC = viewController as? SecondViewController {
-                secondVC.view.backgroundColor = .systemGray
-            } else if let navigationVC = viewController as? UINavigationController {
-                guard let thirdVC = navigationVC.topViewController as? ThirdViewController else { return }
-                thirdVC.title = "Third Screen"
-                thirdVC.view.backgroundColor = .systemCyan
-                
-                }
+        imageView.layer.cornerRadius = 350
+        
+        buttonTapped.layer.cornerRadius = 8
+        
+    
+        
+        func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            guard let tabBarVC = segue.destination as? UITabBarController else { return }
+            guard let viewControllers = tabBarVC.viewControllers else { return }
             
-             }
+            viewControllers.forEach { viewController in
+                if let firstVC = viewController as? FirstViewController {
+                    firstVC.view.backgroundColor = .systemMint
+                } else if let secondVC = viewController as? SecondViewController {
+                    secondVC.view.backgroundColor = .systemGray
+                } else if let navigationVC = viewController as? UINavigationController {
+                    guard let thirdVC = navigationVC.topViewController as? ThirdViewController else { return }
+                    thirdVC.title = "Third Screen"
+                    thirdVC.view.backgroundColor = .systemCyan
+                    
+                }
+                
+            }
             
         }
         
     }
     
-
-
-
-
-
+    
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+    }
+    
+    
+    
+}
